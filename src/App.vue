91faa4f6cb0f02d9fa209a395a3ef3cf208a768b
mc413511928 <413511928@qq.com>
2020-05-28 15:37:10 +0800

    todulist项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" @change="recivedValue" :value="inputValue" class="my_ipt" />
+    <a-button type="primary" @click="addSomeThing">添加事项</a-button>
+
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="cbStatus($event,item.id)">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItem(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{undoneStatus}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="rowkey==='all'?'primary':'default'" @click="changeList('all')">全部</a-button>
+          <a-button :type="rowkey==='undone'?'primary':'default'" @click="changeList('undone')">未完成</a-button>
+          <a-button  :type="rowkey==='done'?'primary':'default'" @click="changeList('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="removeDate">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters(['undoneStatus', 'infoList']),
+    ...mapState(['list', 'inputValue', 'rowkey'])
+
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    ...mapMutations(['addItem', 'setInputValue', 'deleteDate', 'changeStatus', 'unchanged', 'handleChange']),
+    ...mapActions(['getList']),
+    recivedValue (e) {
+      this.setInputValue(e.target.value)
+    },
+    addSomeThing () {
+      if (this.inputValue.trim().length <= 0) {
+        return
+      }
+      this.addItem()
+    },
+    removeItem (id) {
+      this.deleteDate()
+    },
+    cbStatus (e, id) {
+      const params = {
+        id: id,
+        status: e.target.checked
+      }
+      this.changeStatus(params)
+    },
+    removeDate () {
+      this.unchanged()
+    },
+    changeList (key) {
+      this.handleChange(key)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,70 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputValue: 'aaa',
+    nextId: 5,
+    rowkey: 'all'
   },
   mutations: {
+    initList (state, step) {
+      state.list = step
+    },
+    setInputValue (state, value) {
+      state.inputValue = value
+    },
+    addItem (state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    deleteDate (state, id) {
+      const index = state.list.findIndex(t => t.id === id)
+      state.list.splice(index, 1)
+    },
+    changeStatus (state, step) {
+      const f = state.list.find(t => t.id === step.id)
+      f.done = step.status
+    },
+    handleChange (state, step) {
+      state.rowkey = step
+    },
+    unchanged (state) {
+      state.list = state.list.filter(t => t.done === false)
+    }
   },
   actions: {
+    async getList (context) {
+      const { data: res } = await axios.get('/list.json')
+      context.commit('initList', res)
+    }
+
+  },
+  getters: {
+    undoneStatus (state) {
+      const unDon = state.list.filter(t => !t.done)
+      return unDon.length
+    },
+    infoList (state) {
+      if (state.rowkey === 'all') {
+        return state.list
+      } if (state.rowkey === 'undone') {
+        return state.list.filter(t => !t.done)
+      } if (state.rowkey === 'done') {
+        return state.list.filter(t => t.done === true)
+      }
+    }
+
   },
   modules: {
   }
